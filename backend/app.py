4ddf246c85da98bde2b5014065d809ed507602a0
NicolasLepki <nicolaslepki@gmail.com>
2025-06-05 16:00:33 -0400

    fixed flask for render deployment, had it on local server accidentally

--- backend/app.py
+++ backend/app.py
@@ -130,4 +130,5 @@ def data():
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
